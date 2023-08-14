82c79274cead05a2e0f484f8a4807148e3b38fcf
milosnowcat <ramiro.alvarez@rahcode.com>
2023-08-14 17:21:00 -0600

    TODO

---
+++ main.py
@@ -0,0 +1,4 @@
+# TODO hacer un programa que elija un numero random (1-100)
+# y en una venta le pedia al usuario que adivine el número,
+# si no es el número elegido le diara que vuelva a intentarlo,
+# con un número mayor o menor dependiendo de su respuesta
