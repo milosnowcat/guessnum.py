7d98c32c5092c0a6e94a22b91603622918158474
milosnowcat <ramiro.alvarez@rahcode.com>
2023-08-14 20:04:01 -0600

    console

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
 # y en una venta le pedia al usuario que adivine el número,
 # si no es el número elegido le diara que vuelva a intentarlo,
 # con un número mayor o menor dependiendo de su respuesta
+
+import random
+
+number = random.randint(1, 100)
+guess = 0
+
+while guess != number:
+    guess = int(input("What number am I thinking? "))
+
+    if (guess < number):
+        print("Sorry ＞﹏＜, try with a higher number")
+    elif (guess > number):
+        print("Sorry ≧ ﹏ ≦, try with a lower number")
+    else:
+        print("Woo Hoo \^o^/, you guessed the number")
